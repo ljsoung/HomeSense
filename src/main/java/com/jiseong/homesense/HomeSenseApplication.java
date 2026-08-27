379885ad8579b256a5cca28e2ce302b954a274dc
ljsoung <ljsoung0923@sunmoon.ac.kr>
2026-08-27 16:30:40 +0900

    초기 세팅

---
+++ src/main/java/com/jiseong/homesense/HomeSenseApplication.java
@@ -0,0 +1,13 @@
+package com.jiseong.homesense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeSenseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiseong/homesense/HomeSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiseong.homesense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeSenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
