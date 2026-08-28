0802c28e5238fa5a670b46af8952b2f4b5f410eb
ljsoung <ljsoung0923@sunmoon.ac.kr>
2026-08-28 19:56:09 +0900

    feat: user 엔티티 및 레포지토리 추가

---
+++ backend/src/main/java/com/jiseong/homesense/user/entity/UserRole.java
@@ -0,0 +1,6 @@
+package com.jiseong.homesense.user.entity;
+
+public enum UserRole {
+    USER,
+    ADMIN
+}
---
+++ backend/src/main/java/com/jiseong/homesense/user/entity/UserStatus.java
@@ -0,0 +1,7 @@
+package com.jiseong.homesense.user.entity;
+
+public enum UserStatus {
+    ACTIVE,
+    SUSPENDED,
+    WITHDRAWN
+}
